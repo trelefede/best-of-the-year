682b712f9b4b466c1f4380e122755914fbb1ba3a
TreleaniFederico <treleani.federico@gmail.com>
2023-02-10 16:58:10 +0100

    correct syntax errors

--- src/main/java/org/lessons/java/bestoftheyear/controller/MainController.java
+++ src/main/java/org/lessons/java/bestoftheyear/controller/MainController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 
 public class MainController {
 	@GetMapping()
-	@ResponseBody
-	public String index(Model model) {
+	public String home(Model model) {
 		model.addAttribute("myName", "bestemmie");
 		return "index";
 	}
